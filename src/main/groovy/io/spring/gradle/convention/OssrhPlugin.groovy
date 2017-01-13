e78136ffa9f0ab9921d25e991950c9079f579daa
Rob Winch <rwinch@gopivotal.com>
2017-01-13 17:11:26 -0600

    Conditionally use ossrhUsername

--- src/main/groovy/io/spring/gradle/convention/OssrhPlugin.groovy
+++ src/main/groovy/io/spring/gradle/convention/OssrhPlugin.groovy
@@ -7,15 +7,17 @@ public class OssrhPlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
-		project.uploadArchives {
-			repositories {
-				mavenDeployer {
-					repository(url: "https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
-						authentication(userName: project.ossrhUsername, password: project.ossrhPassword)
-					}
+		if(project.hasProperty('ossrhUsername')) {
+			project.uploadArchives {
+				repositories {
+					mavenDeployer {
+						repository(url: "https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+							authentication(userName: project.ossrhUsername, password: project.ossrhPassword)
+						}
 
-					snapshotRepository(url: "https://oss.sonatype.org/content/repositories/snapshots/") {
-						authentication(userName: project.ossrhUsername, password: project.ossrhPassword)
+						snapshotRepository(url: "https://oss.sonatype.org/content/repositories/snapshots/") {
+							authentication(userName: project.ossrhUsername, password: project.ossrhPassword)
+						}
 					}
 				}
 			}
