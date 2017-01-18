58f99502c88945d32df329056d8f34b2c8b930e5
Rob Winch <rwinch@gopivotal.com>
2017-01-18 16:02:59 -0600

    Add support for dependency-management.properties

--- src/main/java/io/spring/gradle/convention/DependencyManagementExportTask.java
+++ src/main/java/io/spring/gradle/convention/DependencyManagementExportTask.java
@@ -1,11 +1,11 @@
 package io.spring.gradle.convention;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.Properties;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.artifacts.Configuration;
@@ -14,24 +14,26 @@ import org.gradle.api.tasks.TaskAction;
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension;
 
 public class DependencyManagementExportTask extends DefaultTask {
-	PrintWriter writer = new PrintWriter(System.out);
+	private OutputStream output;
 
 	@TaskAction
 	public void dependencyManagementExport() throws IOException {
+		if(output == null) {
+			output = new FileOutputStream(getProject().getRootProject().file("gradle/dependency-management.properties"));
+		}
+
 		DependencyManagementExtension dependencyMgmt = (DependencyManagementExtension) getProject().getExtensions().getByName("dependencyManagement");
 
 		Configuration springIoTestRuntime = getProject().getConfigurations().getByName("springIoTestRuntime");
 		Map<String, String> versionsForConfiguration = dependencyMgmt.getManagedVersionsForConfiguration(springIoTestRuntime);
-		Map<String, String> managedVersions = new TreeMap<String,String>(versionsForConfiguration);
 
-		writer.println("# Export from " + getProject().findProperty("springIoVersion"));
-		for(Map.Entry<String, String> entry : managedVersions.entrySet()) {
-			writer.println(entry.getKey() + "=" + entry.getValue());
-		}
-		writer.flush();
+		Properties properties = new SortedProperties();
+		properties.putAll(versionsForConfiguration);
+
+		properties.store(output, "Export from " + getProject().findProperty("springIoVersion"));
 	}
 
 	void setOutputFile(File file) throws IOException {
-		this.writer = new PrintWriter(new FileWriter(file));
+		this.output = new FileOutputStream(file);
 	}
 }
