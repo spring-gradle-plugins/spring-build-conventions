48c1fd9ff88ea1fc365924a0ca042c28d358a180
Rob Winch <rwinch@gopivotal.com>
2017-01-20 16:22:13 -0600

    Add RootProjectPlugin / Sonar support

---
+++ src/main/groovy/io/spring/gradle/convention/RootProjectPlugin.groovy
@@ -0,0 +1,30 @@
+package io.spring.gradle.convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.PluginManager
+
+public class RootProjectPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		PluginManager pluginManager = project.getPluginManager()
+		pluginManager.apply(SchemaPlugin)
+		pluginManager.apply("org.sonarqube")
+
+		String projectName = Utils.getProjectName(project);
+		project.sonarqube {
+			properties {
+				property "sonar.java.coveragePlugin", "jacoco"
+				property "sonar.projectName", projectName
+				property "sonar.jacoco.reportPath", "${project.buildDir.name}/jacoco.exec"
+				property "sonar.links.homepage", "https://spring.io/${projectName}"
+				property "sonar.links.ci", "https://jenkins.spring.io/job/${projectName}/"
+				property "sonar.links.issue", "https://github.com/spring-projects/${projectName}/issues"
+				property "sonar.links.scm", "https://github.com/spring-projects/${projectName}"
+				property "sonar.links.scm_dev", "https://github.com/spring-projects/${projectName}.git"
+				property "sonar.java.coveragePlugin", "jacoco"
+			}
+		}
+	}
+}
