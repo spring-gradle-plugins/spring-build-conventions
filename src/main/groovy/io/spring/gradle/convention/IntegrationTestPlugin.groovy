895956f545a5d76d3b5d0883a9ca792dddec384a
Rob Winch <rwinch@users.noreply.github.com>
2017-02-10 20:03:53 -0600

    Eclipse += integrationTestRuntime

--- src/main/groovy/io/spring/gradle/convention/IntegrationTestPlugin.groovy
+++ src/main/groovy/io/spring/gradle/convention/IntegrationTestPlugin.groovy
@@ -7,6 +7,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.testing.Test
+import org.gradle.plugins.ide.eclipse.EclipsePlugin
 
 public class IntegrationTestPlugin implements Plugin<Project> {
 
@@ -48,6 +49,12 @@ public class IntegrationTestPlugin implements Plugin<Project> {
 				integrationTest {
 					groovy.srcDirs project.file('src/integration-test/groovy')
 				}
+			}
+		}
+
+		project.plugins.withType(EclipsePlugin) {
+			project.eclipse.classpath {
+					plusConfigurations += [ project.configurations.integrationTestRuntime ]
 			}
 		}
 	}
