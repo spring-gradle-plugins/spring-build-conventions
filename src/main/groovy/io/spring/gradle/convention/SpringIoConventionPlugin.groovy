e511677ecdbd8e7899b6439cf9f628324e321421
Rob Winch <rwinch@users.noreply.github.com>
2017-04-13 22:00:08 -0500

    Add SpringIo override

--- src/main/groovy/io/spring/gradle/convention/SpringIoConventionPlugin.groovy
+++ src/main/groovy/io/spring/gradle/convention/SpringIoConventionPlugin.groovy
@@ -1,5 +1,6 @@
 package io.spring.gradle.convention
 
+import io.spring.gradle.dependencymanagement.DependencyManagementPlugin
 import io.spring.gradle.propdeps.PropDepsPlugin
 import io.spring.gradle.springio.SpringIoPlugin
 
@@ -9,12 +10,24 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 public class SpringIoConventionPlugin implements Plugin<Project> {
+	static final String DEPENDENCY_MANAGEMENT_RESOURCE = "gradle/springio-dependency-management.properties"
 
 	@Override
 	public void apply(Project project) {
 		project.ext['JDK8_HOME'] = System.getProperty("java.home");
 		project.getPluginManager().apply(SpringIoPlugin)
 
+		project.plugins.withType(SpringIoPlugin) {
+			project.configurations.withType(Object.class) { c->
+				if(c.getName() == 'springIoTestRuntime') {
+					println c.getClass()
+					applySpringIoConfiguration(project)
+				}
+			}
+		}
+	}
+
+	private void applySpringIoConfiguration(Project project) {
 		String platformBomVersion = project.springIoVersion
 		if(project.hasProperty('platformVersion')) {
 			platformBomVersion = project.findProperty("platformVersion");
@@ -32,6 +45,29 @@ public class SpringIoConventionPlugin implements Plugin<Project> {
 			project.configurations {
 				springIoTestRuntime {
 					extendsFrom optional, provided
+				}
+			}
+		}
+
+		applyDependencyManagementWith(project, project.rootProject.file(DEPENDENCY_MANAGEMENT_RESOURCE))
+		applyDependencyManagementWith(project, project.file(DEPENDENCY_MANAGEMENT_RESOURCE))
+	}
+
+	public void applyDependencyManagementWith(Project project, File dependencyManagementFile) {
+		if(!dependencyManagementFile.exists()) {
+			return;
+		}
+
+		Properties dependencyMap = new Properties();
+		dependencyMap.load(new FileInputStream(dependencyManagementFile));
+
+		project.dependencyManagement {
+			springIoTestRuntime {
+				dependencies {
+					for (Map.Entry<Object, Object> entry : dependencyMap.entrySet()) {
+						String managedDependency = entry.getKey() + ":" + entry.getValue();
+						dependency managedDependency
+					}
 				}
 			}
 		}
