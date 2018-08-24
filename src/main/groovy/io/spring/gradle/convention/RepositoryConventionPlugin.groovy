3755e7b5271ca686ff1b63c6bb08b915ff3c3e9b
Vedran Pavic <vedran.pavic@gmail.com>
2018-08-24 21:54:28 +0200

    Add support for force enabling repositories
    By default, dependency resolution repository is configured according to project version, i.e. release, milestone or snapshot. This commit adds support for force enabling milestone or snapshot repository, and in addition to that for adding local Maven repository.
    
    Closes gh-34

--- src/main/groovy/io/spring/gradle/convention/RepositoryConventionPlugin.groovy
+++ src/main/groovy/io/spring/gradle/convention/RepositoryConventionPlugin.groovy
@@ -1,25 +1,51 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.spring.gradle.convention;
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class RepositoryConventionPlugin implements Plugin<Project> {
+class RepositoryConventionPlugin implements Plugin<Project> {
+
 	@Override
-	public void apply(Project project) {
+	void apply(Project project) {
 		boolean isSnapshot = Utils.isSnapshot(project)
-		boolean isMilestone = Utils.isMilestone(project);
+		boolean isMilestone = Utils.isMilestone(project)
+
+		String[] forceMavenRepositories = ((String) project.findProperty("forceMavenRepositories"))?.split(',')
 
 		String mavenUrl
-		if(isSnapshot) {
-			mavenUrl = 'https://repo.spring.io/libs-snapshot'
-		} else if(isMilestone) {
-			mavenUrl = 'https://repo.spring.io/libs-milestone'
-		} else {
-			mavenUrl = 'https://repo.spring.io/libs-release';
+		if (isSnapshot || forceMavenRepositories?.contains('snapshot')) {
+			mavenUrl = 'https://repo.spring.io/libs-snapshot/'
+		}
+		else if (isMilestone || forceMavenRepositories?.contains('milestone')) {
+			mavenUrl = 'https://repo.spring.io/libs-milestone/'
+		}
+		else {
+			mavenUrl = 'https://repo.spring.io/libs-release/'
 		}
 
 		project.repositories {
+			if (forceMavenRepositories?.contains('local')) {
+				mavenLocal()
+			}
 			maven { url mavenUrl }
 		}
+
 	}
+
 }
