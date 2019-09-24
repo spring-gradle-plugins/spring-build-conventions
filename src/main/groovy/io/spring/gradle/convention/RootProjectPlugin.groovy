0bb3dd1929e9da000ff025b129e33b475183cb69
Vedran Pavic <vedran.pavic@gmail.com>
2019-09-24 23:56:27 +0200

    Disable caching of changing modules and dynamic versions

--- src/main/groovy/io/spring/gradle/convention/RootProjectPlugin.groovy
+++ src/main/groovy/io/spring/gradle/convention/RootProjectPlugin.groovy
@@ -34,6 +34,15 @@ class RootProjectPlugin implements Plugin<Project> {
 
 		project.repositories.mavenCentral()
 
+		project.allprojects {
+			configurations.all {
+				resolutionStrategy {
+					cacheChangingModulesFor 0, "seconds"
+					cacheDynamicVersionsFor 0, "seconds"
+				}
+			}
+		}
+
 		String projectName = Utils.getProjectName(project)
 		project.sonarqube {
 			properties {
